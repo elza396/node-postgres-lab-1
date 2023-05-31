f58c5bd1de1abb095cd853138424f4d643833f67
Elza Gazizova <elza396@yandex.ru>
2023-06-01 01:03:23 +0500

    init project add server

---
+++ server/.prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    trailingComma: 'none',
+    tabWidth: 4,
+    semi: false,
+    singleQuote: true,
+    bracketSpacing: false,
+    arrowParens: 'avoid',
+    printWidth: 144,
+    jsxBracketSameLine: true
+}
+
