fd51d91203e46d155eb021f510591b25b9af5c52
Elza Gazizova <elza396@yandex.ru>
2023-06-01 01:31:14 +0500

    init client CRA

---
+++ client/.prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    trailingComma: 'none',
+    tabWidth: 4,
+    semi: false,
+    singleQuote: true,
+    bracketSpacing: false,
+    arrowParens: 'avoid',
+    printWidth: 144,
+    jsxBracketSameLine: true
+}
---
+++ client/src/App.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import s from './App.module.css'
+
+function App() {
+    return <div className={s.App} />
+}
+
+export default App
